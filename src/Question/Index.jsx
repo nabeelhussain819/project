ccaab575f3acd49b361d999d8df01cb80b102997
nabeelhussain819 <hnabeel570@gmail>
2022-03-07 22:49:13 +0500

    :

--- src/Question/Index.jsx
+++ src/Question/Index.jsx
@@ -1,36 +1,15 @@
 import React, { useState, useEffect } from "react";
 import myData from "./question.json";
 import "./question.css";
+import Hello from "./Check";
 
 function Index() {
-  let count = 1;
+
+ 
 
   return (
     <>
-      {myData.map((item) => {
-        return (
-          <>
-            <div class="card">
-              <div class="container">
-              <h1 className="num">Question No:{count++}</h1>{" "}
-              <p className="type">{unescape(item.category)}</p>
-              <h3>{unescape(item.question)}</h3>
-            {item.incorrect_answers.map((data)=>{
-                return(
-                    <>
-                    <p className="answers">{unescape(data)}</p>
-                    
-                    </>
-                )
-            })}<p >{unescape(item.correct_answer)}</p>
-              </div>
-            </div>
-            
-         
-           
-          </>
-        );
-      })}
+      <Hello />
     </>
   );
 }
